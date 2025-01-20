412b884d7f996b04d3d2e952b340b1ccad56cc3a
SwedishHouse <swedish2015@yandex.ru>
2025-01-20 20:05:59 +0400

    test connection

--- main_client.cpp
+++ main_client.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
-#include "action_handler.h"
+#include <cstring>
+#include <unistd.h>
+#include <linux/netlink.h>
+#include <linux/genetlink.h>
+#include <sys/socket.h>
 
+#define GENL_TEST_FAMILY_NAME "genl_test"
+#define GENL_TEST_MCGRP_NAME "genl_test_mcgrp"
 
-int main()
-{
-    ActionHandler act_handler;
-    int arg1 = 5, arg2 = 2;
-    std:: cout << "Hello world from client" << std::endl;
-    
+int main() {
+    struct sockaddr_nl sa;
+    struct nlmsghdr *nlh;
+    struct genlmsghdr *genlh;
+    int sock_fd;
+    std::string json_str;
+
+    std::cout << "Client starts..." << std::endl;
+
+    sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+    if (sock_fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+
+    while (std::getline(std::cin, json_str)) {
+        // usleep(500000); // 0.5 sec
+        nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(GENL_HDRLEN + json_str.size() + 1));
+        nlh->nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN + json_str.size() + 1);
+        nlh->nlmsg_type = GENL_ID_CTRL;
+        nlh->nlmsg_flags = NLM_F_REQUEST;
+        nlh->nlmsg_seq = 0;
+        nlh->nlmsg_pid = getpid();
+
+        genlh = (struct genlmsghdr *)NLMSG_DATA(nlh);
+        genlh->cmd = 0;
+        genlh->version = 1;
+
+        strcpy((char *)NLMSG_DATA(nlh) + GENL_HDRLEN, json_str.c_str());
+
+        if (sendto(sock_fd, nlh, nlh->nlmsg_len, 0, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+            perror("sendto");
+            close(sock_fd);
+            return -1;
+        }
+
+        std::cout << "JSON string sent: " << json_str << std::endl;
+
+        // Получение ответа от сервера
+        int len = recv(sock_fd, nlh, NLMSG_SPACE(256), 0);
+        if (len < 0) {
+            perror("recv");
+            close(sock_fd);
+            return -1;
+        }
+
+        std::string response_json = (char *)NLMSG_DATA(nlh) + GENL_HDRLEN;
+        std::cout << "Received response JSON string: " << response_json << std::endl;
+
+        
+        free(nlh);
+    }
+
+    close(sock_fd);
     return 0;
 }
--- main_server.cpp
+++ main_server.cpp
@@ -1,14 +1,68 @@
 #include <iostream>
-#include "action_handler.h"
+#include <cstring>
+#include <unistd.h>
+#include <linux/netlink.h>
+#include <linux/genetlink.h>
+#include <sys/socket.h>
 
+#define GENL_TEST_FAMILY_NAME "genl_test"
+#define GENL_TEST_MCGRP_NAME "genl_test_mcgrp"
 
-int main()
-{
-    ActionHandler act_handler;
-    int arg1 = 5, arg2 = 2;
-    std:: cout << "Hello world from server" << std::endl;
-    std:: cout << "Add: " << act_handler.execute<int>(ActionHandler::Action::ADD, arg1, arg2) << std::endl;
-    std:: cout << "Sub: " << act_handler.execute<int>(ActionHandler::Action::SUBSTRACT, arg1, arg2) << std::endl;
-    std:: cout << "Mult: " << act_handler.execute<int>(ActionHandler::Action::MULTIPLY, arg1, arg2) << std::endl;
+int main() {
+    struct sockaddr_nl sa, client_sa;
+    struct nlmsghdr *nlh;
+    int sock_fd;
+    char buffer[256];
+
+    std::cout << "Server starts..." << std::endl;
+
+    sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+    if (sock_fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+    sa.nl_pid = getpid();
+    sa.nl_groups = 0x0;
+
+    if (bind(sock_fd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+        perror("bind");
+        close(sock_fd);
+        return -1;
+    }
+    
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(256));
+
+    std::cout << "Server ready!" << std::endl;
+
+    while (true) {
+        socklen_t client_sa_len = sizeof(client_sa);
+        int len = recv(sock_fd, nlh, NLMSG_SPACE(256), 0);
+
+        std::cout << "Received data length: " << len << std::endl;
+
+        if (len < 0) {
+            perror("recv");
+            close(sock_fd);
+            return -1;
+        }
+        std::string received_json = (char *)NLMSG_DATA(nlh) + GENL_HDRLEN;
+        std::cout << "Received JSON string: " << received_json << std::endl;
+
+        // Отправка полученного сообщения обратно клиенту
+        nlh->nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN + received_json.size() + 1);
+        strcpy((char *)NLMSG_DATA(nlh) + GENL_HDRLEN, received_json.c_str());
+
+        if (sendto(sock_fd, nlh, nlh->nlmsg_len, 0, (struct sockaddr *)&client_sa, client_sa_len) < 0) {
+            perror("sendto");
+            close(sock_fd);
+            return -1;
+        }
+
+    }
+
+    close(sock_fd);
     return 0;
 }
